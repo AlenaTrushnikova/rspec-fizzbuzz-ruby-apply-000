bbf11e1d62329806d46c89a96f3fa441c7444963
Alena Trushnikova <alena.yu.t@gmail.com>
2020-09-10 18:11:27 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,10 @@
 
 def fizzbuzz(int)
   if int % 3 && int % 5 == 0 
-    return "Fizz"
+    return "FizzBuzz"
   elsif int % 5 == 0
     return "Buzz"
-  elsif int % 3 == 0 && int % 5 == 0
-    return "FizzBuzz"
+  elsif int % 3 == 0
+    return "Fizz"
   end
 end 
